7afb881b580116f5865d2d45293b4a931cf15b62
Microwrk <microwrk.online@gmail.com>
2025-04-09 07:11:08 +0530

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React from "react";
-import Home from "../src/pages/Home";  // ✅ Importing Home correctly
+import React, { useEffect } from "react";
+import {
+  useLocation,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "../src/pages/Home";
 
-const App = () => {
+// Custom hook to send page views to GA4
+const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-QT9PZ7HNHY", {
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
+};
+
+const AppContent = () => {
+  usePageTracking();
+
   return (
     <div className="min-h-screen bg-gray-900">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add more routes here if needed */}
+      </Routes>
     </div>
   );
 };
+
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
 
 export default App;
--- src/components/ChapterList.jsx
+++ src/components/ChapterList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import DownloadButton from "./DownloadButton";
-// import "../index.css"; // Import the index.css where Tailwind CSS is set up
 
 const ChapterList = ({ chapters, files }) => {
   return (
