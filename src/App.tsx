203406cbb756dc4a35f6b1bd8df86830ad53c879
Microwrk <microwrk.online@gmail.com>
2025-04-12 14:22:56 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,30 @@ import Footer from "./components/homepage/Footer";
 import WhyMicrowrk from "./components/homepage/WhyMicrowrk";
 import PrivacyPolicy from "./components/footercontents/Privacy";
 import TermsOfService from "./components/footercontents/Terms";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <Navbar />
-            <Hero />
-            <FeaturedTools />
-            <WhyMicrowrk />
-            <Footer />
-          </>
-        }
-      />
-      <Route path="/privacy" element={<PrivacyPolicy />} />
-      <Route path="/terms" element={<TermsOfService />} />
-    </Routes>
+    <>
+    {/* Vercel Analytics */}
+      <Analytics />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <FeaturedTools />
+              <WhyMicrowrk />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
+      </Routes>
+    </>
   );
 }
 
