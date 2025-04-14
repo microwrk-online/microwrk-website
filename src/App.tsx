556d6a8a344e9062ea6b9d54a461d8aa44774208
Microwrk <microwrk.online@gmail.com>
2025-04-14 10:35:39 +0530

    testing route - jd

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsOfService />} />
         <Route
-          path="/youtube-chapter-downloader"
+          path="/youtubechapterdownloader"
           element={<YouTubeChapterDownloader />}
         />
         <Route
